73f9cb084d88a1c1efbd09373089df2ee106735b
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 14:35:25 -0300

    Add project files.

---
+++ BomDiaTardeNoite/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Buffers.Binary;
+
+namespace BomDiaTardeNoite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a hora atual? (HH)");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+                Console.WriteLine("Bom dia!");
+            else if (hora < 18)
+                Console.WriteLine("Boa tarde!");
+            else
+                Console.WriteLine("Boa noite!");
+        }
+    }
+}
