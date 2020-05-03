714fcfae3052bf7fb84b9dcae9570639f7190b1b
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 16:16:38 -0300

    Adicionando tratamento de excessão

--- BomDiaTardeNoite/Program.cs
+++ BomDiaTardeNoite/Program.cs
@@ -7,15 +7,28 @@ namespace BomDiaTardeNoite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qual a hora atual? (HH)");
-            int hora = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Qual a hora atual? (HH)");
+                int hora = int.Parse(Console.ReadLine());
 
-            if (hora < 12)
-                Console.WriteLine("Bom dia!");
-            else if (hora < 18)
-                Console.WriteLine("Boa tarde!");
-            else
-                Console.WriteLine("Boa noite!");
+                if (hora < 12)
+                    Console.WriteLine("Bom dia!");
+                else if (hora < 18)
+                    Console.WriteLine("Boa tarde!");
+                else if(hora<25)
+                    Console.WriteLine("Boa noite!");
+                else
+                    Console.WriteLine("Hora inválida");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro de formatação: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
+            }
         }
     }
 }
